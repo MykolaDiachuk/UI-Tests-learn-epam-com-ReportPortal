e0f05e8360b4df8dc8732bb15bb320fbcd8cf1c6
MykolaDiachuk <diachuk.mykola2005@gmail.com>
2025-04-09 19:48:25 +0300

    Fix to comply with the SOLID

--- src/main/java/org/example/demo/listeners/TestListener.java
+++ src/main/java/org/example/demo/listeners/TestListener.java
@@ -3,8 +3,6 @@ package org.example.demo.listeners;
 import io.qameta.allure.listener.TestLifecycleListener;
 import io.qameta.allure.model.Status;
 import io.qameta.allure.model.TestResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.example.demo.utils.reporting.AllureAttachmentService.attachLogs;
 import static org.example.demo.utils.reporting.AllureAttachmentService.attachScreenshot;
